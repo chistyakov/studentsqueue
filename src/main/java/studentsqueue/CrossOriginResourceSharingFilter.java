b4e5b178ee6321db95c279c4e04b872cb5a2f64a
chistyakov <al.ol.chistyakov@gmail.com>
2014-06-05 00:04:08 +0400

    added CrossOriginResourceSharingFilter to allow XSS

---
+++ src/main/java/studentsqueue/CrossOriginResourceSharingFilter.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package studentsqueue;
+
+import com.sun.jersey.spi.container.ContainerRequest;
+import com.sun.jersey.spi.container.ContainerResponse;
+import com.sun.jersey.spi.container.ContainerResponseFilter;
+
+/**
+ *
+ * @author teamdevelopment
+ */
+public class CrossOriginResourceSharingFilter implements ContainerResponseFilter {
+
+    @Override
+    public ContainerResponse filter(ContainerRequest request, ContainerResponse response) {
+        response.getHttpHeaders().putSingle("Access-Control-Allow-Origin", "*");
+        response.getHttpHeaders().putSingle("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+        response.getHttpHeaders().putSingle("Access-Control-Allow-Headers", "content-type");
+        return response;
+    }
+    
+}

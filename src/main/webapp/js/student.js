aa029355bd976cbef3e3473c569f2fcdc5692713
soylent-grin <hocico16@gmail.com>
2014-06-11 01:36:32 +0400

    Normalize teacher's show/hide algorithm

--- src/main/webapp/js/student.js
+++ src/main/webapp/js/student.js
@@ -149,12 +149,13 @@
                     for (var i = 0; i < $scope.queues.length; i++) {
                         if ($scope.queues[i].teacher_id == item.id) {
                             show = true; 
-                        }
-                        for (var j = 0; j < $scope.queues[i].students.length; j++) {
-                            if ($scope.queues[i].students[j].student_id == $scope.current_student.id) {
-                                show = false;
+                            for (var j = 0; j < $scope.queues[i].students.length; j++) {
+                                if ($scope.queues[i].students[j].student_id == $scope.current_student.id) {
+                                    show = false;
+                                }
                             }
                         }
+                        
                     } 
                     return show;
                 };

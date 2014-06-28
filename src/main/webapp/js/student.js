f08a59523d5481759bee6204c1643eac0a42b12c
soylent-grin <hocico16@gmail.com>
2014-06-28 20:11:15 +0400

    FIX

--- src/main/webapp/js/student.js
+++ src/main/webapp/js/student.js
@@ -229,8 +229,9 @@
                 success: function() {
                     window.location= BASE_OF_BASE_URL + "login.html";
                 }
-				window.location= BASE_OF_BASE_URL + "login.html";
-            })
+				
+            });
+			window.location= BASE_OF_BASE_URL + "login.html";
         });
         $('.logout').attr('href', BASE_URL + 'auth/logout');
     })
